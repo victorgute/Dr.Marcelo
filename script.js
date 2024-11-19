6b7727543a6a48b4ec49976147419879a3eb4178
victor <victor.h.gutierrez.carvalho>
2024-11-19 17:04:58 -0300

    terceiro

---
+++ script.js
@@ -0,0 +1,23 @@
+document.querySelectorAll('.cta-button').forEach(button => {
+    button.addEventListener('click', () => {
+        window.open('https://wa.me/16997015939', '_blank');
+    });
+});
+
+const backToTopButton = document.getElementById('backToTop');
+
+window.onscroll = function() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        backToTopButton.classList.add('show');
+    } else {
+        backToTopButton.classList.remove('show');
+    }
+};
+
+backToTopButton.addEventListener('click', function(e) {
+    e.preventDefault();
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
